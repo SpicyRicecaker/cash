19fb31725ee7d1ec6d01cebed47c515c394982f5
SpicyRicecaker <awesomenessisgolden@gmail.com>
2021-07-21 18:18:45 -0700

    edit modes

--- src/routes/books/index.svelte
+++ src/routes/books/index.svelte
@@ -43,122 +43,106 @@
 	import type { Book } from '$lib/types';
 	export let books: Book[];
 
-	let start: number;
-	let inUpdate: boolean = false;
-	const RESPONSE_TIME: number = 300;
-	let elapsed: number = RESPONSE_TIME;
-	$: progress = Math.min(elapsed / RESPONSE_TIME, RESPONSE_TIME);
+	let selected: number = -1;
+	let editMode: boolean = false;
 
-	function loop(timestamp: number) {
-		elapsed = timestamp - start;
-		if (elapsed < RESPONSE_TIME) {
-			// After 1000 ms of no changes update
-			window.requestAnimationFrame(loop);
-		} else {
-			console.log('sending');
-			fetch('/user/books', {
-				method: 'POST',
-				headers: { 'Content-Type': 'application/json' },
-				body: JSON.stringify(books)
-			}).finally(() => (inUpdate = false));
-		}
-	}
-	let booksValidator: [boolean, boolean, boolean, boolean][] = Array(books.length).fill([
-		true,
-		true,
-		true,
-		false
-	]);
+	let booksValidator: [boolean, boolean, boolean, boolean] = [true, true, true, false];
 
-	let first: boolean = true;
-
-	$: sendBook(books);
-	function sendBook(_: Book[]) {
-		if (first) {
-			first = !first;
-			return;
-		}
-		start = performance.now();
-		if (!inUpdate) {
-			inUpdate = !inUpdate;
-			window.requestAnimationFrame(loop);
-		}
-	}
 	function delBook(idx: number) {
-		// books = [...books.slice(0, idx), ...books.slice(idx + 1)];
-		booksValidator = [...booksValidator.slice(0, idx), ...booksValidator.slice(idx + 1)];
+		books = [...books.slice(0, idx), ...books.slice(idx + 1)];
+		// booksValidator = [...booksValidator.slice(0, idx), ...booksValidator.slice(idx + 1)];
 	}
-	// function addBook() {
-	// 	books = [...books, bookDefault()];
-	// 	booksValidator = [...booksValidator, [true, true, true, false]];
-	// }
+	function addBook() {
+		// books = [...books, bookDefault()];
+		// 	booksValidator = [...booksValidator, [true, true, true, false]];
+	}
 	function selBook(book: Book) {
 		$selectedBook = book;
 	}
 
 	// e.g. ('innerHTML', i where i <= books.length, 0 | 1 | 2)
-	const handleTypeInputChange = (t: string, i: number, field: number) =>
-		(booksValidator[i][field] = t == 'innerHTML' || t == 'selector');
+	const handleTypeInputChange = (t: string, field: number) =>
+		(booksValidator[field] = t == 'innerHTML' || t == 'selector');
 </script>
 
 <div>
-	<div>
-		<progress class={inUpdate ? 'progressing' : 'regressing'} value={progress} />
-	</div>
+	{#if selected != -1}
+		<div id="form">
+			<label for="name">name</label>
+			<input bind:value={books[selected].name} id="name" />
+
+			<label for="url">name</label>
+			<input bind:value={books[selected].url} id="url" />
+
+			<label for="content-type">content type</label>
+			<input
+				on:keyup={() => handleTypeInputChange(books[selected].content.type, 0)}
+				bind:value={books[selected].content.type}
+				class={booksValidator[0] ? 'valid' : 'invalid'}
+				id="content-type"
+			/>
+
+			<label for="content-value">content value</label>
+			<input bind:value={books[selected].content.value} id="content-value" />
+
+			<label for="prev-chapter-type">prev chapter type</label>
+			<input
+				on:keyup={() => handleTypeInputChange(books[selected].prevChapter.type, 1)}
+				bind:value={books[selected].prevChapter.type}
+				class={booksValidator[1] ? 'valid' : 'invalid'}
+				id="prev-chapter-type"
+			/>
+
+			<label for="prev-chapter-value">prev chapter value</label>
+			<input bind:value={books[selected].prevChapter.value} id="prev-chapter-value" />
+
+			<label for="next-chapter-type">next chapter type</label>
+			<input
+				on:keyup={() => handleTypeInputChange(books[selected].nextChapter.type, 2)}
+				bind:value={books[selected].nextChapter.type}
+				class={booksValidator[0] ? 'valid' : 'invalid'}
+				id="next-chapter-type"
+			/>
+
+			<label for="next-chapter-value">next chapter value</label>
+			<input bind:value={books[selected].nextChapter.value} id="next-chapter-value" />
+
+			<button>✕</button>
+			<button>Submit</button>
+		</div>
+	{/if}
 	<div id="grid">
 		{#each books as book, i}
 			<div class="book">
-				<h2><span bind:innerHTML={book.name} contenteditable="true" /></h2>
-				<div>Sourced from: <span bind:innerHTML={book.url} contenteditable="true" /></div>
-				<div>
-					<div>
-						Content Type <input
-							on:keyup={() => handleTypeInputChange(book.content.type, i, 0)}
-							bind:value={book.content.type}
-							class={booksValidator[i][0] ? 'valid' : 'invalid'}
-						/>
-					</div>
-					<div>
-						Content Value <span bind:innerHTML={book.content.value} contenteditable="true" />
-					</div>
-					<div>
-						Prev Chapter Type <input
-							on:keyup={() => handleTypeInputChange(book.prevChapter.type, i, 1)}
-							bind:value={book.prevChapter.type}
-							class={booksValidator[i][1] ? 'valid' : 'invalid'}
-						/>
-					</div>
-					<div>
-						Prev Chapter Value <span
-							bind:innerHTML={book.prevChapter.value}
-							contenteditable="true"
-						/>
-					</div>
-					<div>
-						Next Chapter Type <input
-							on:keyup={() => handleTypeInputChange(book.nextChapter.type, i, 2)}
-							bind:value={book.nextChapter.type}
-							class={booksValidator[i][2] ? 'valid' : 'invalid'}
-						/>
-					</div>
-					<div>
-						Next Chapter Value <span
-							bind:innerHTML={book.nextChapter.value}
-							contenteditable="true"
-						/>
-					</div>
-				</div>
-				<button on:click={() => delBook(i)}>delete</button>
+				<h2>{book.name}</h2>
+				<div>{book.url} /></div>
 				<a href="/books/read"><button on:click={() => selBook(book)}>Read!</button></a>
+				{#if selected != -1}
+					<button on:click={() => delBook(i)}>✕</button>
+				{/if}
 			</div>
 		{/each}
-		<div>
-			<button on:click={() => addBook()}>add</button>
-		</div>
+		{#if selected != -1}
+			<div class="book">
+				<button on:click={() => addBook()}>add</button>
+			</div>
+		{/if}
 	</div>
+	{#if editMode}
+		<button on:click={() => (editMode = !editMode)}>Edit Mode</button>
+	{:else}
+		<button on:click={() => (editMode = !editMode)}>No More Edit Mode</button>
+	{/if}
 </div>
 
 <style lang="scss">
+	#form {
+		position: absolute;
+		z-index: 150;
+		background-color: var(--background-color);
+		width: 100%;
+		height: 100%;
+	}
 	#grid {
 		display: grid;
 		grid-gap: 1rem;
